6b82b33ab9009ae5f2c82dd87ce2464571fd0f53
Alex Mattson <amattson21@gmail.com>
2016-08-03 11:53:47 -0700

    completed user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe "validations" do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_presence_of(:session_token) }
     it { should validate_length_of(:password).is_at_least(6) }
   end
 
@@ -16,11 +28,11 @@ RSpec.describe User, type: :model do
     let(:user) { create(:user) }
 
     it "finds valid user" do
-      expect(User.find_by_credentials(user.username, user.password)).to be(user)
+      expect(User.find_by_credentials(user.username, user.password).username).to eq(user.username)
     end
 
     it "does not find invalid user" do
-      expect(User.find_by_credentials(user.username, "")).not_to be(user)
+      expect(User.find_by_credentials(user.username, "")).to be_nil
     end
   end
 end
